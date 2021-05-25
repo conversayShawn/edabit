cc2055cb8c39f1b15c16f0355d14ffb942d8cb04
Shawn <shawn.harris3510@gmail.com>
2021-05-25 19:57:57 -0400

    Pair Management - failed

--- very_easy/app.js
+++ very_easy/app.js
@@ -10,3 +10,9 @@ const convert = (h, m) => ((h*60)+m)*60
 // Compare Strings by Count of Characters
 const comp = (str1, str2) => str1.length === str2.length ? true : false
 const comp = (str1, str2) => str1.length === str2.length // refactor
+
+// Given two arguments, return an array which contains these two arguments.
+const makePair = (num1, num2) => {
+	let arr = []
+	return arr.push(num1, ...num2)
+}
