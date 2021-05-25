da5c06fc0dfed2a233cd125ed10966e93412d510
Shawn <shawn.harris3510@gmail.com>
2021-05-25 19:08:01 -0400

    Convert Hours and Minutes into Seconds

--- very_easy/app.js
+++ very_easy/app.js
@@ -3,3 +3,6 @@ const addition = (a, b) => a + b
 
 // Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
 const frames = (min, fps) => min * fps * 60
+
+// Convert Hours and Minutes into Seconds
+const convert = (h, m) => ((h*60)+m)*60
