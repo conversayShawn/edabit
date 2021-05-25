c7206227dfe317668df9b8d03a141365072cd852
Shawn <shawn.harris3510@gmail.com>
2021-05-24 21:50:24 -0400

    return the frames per second

--- very_easy/app.js
+++ very_easy/app.js
@@ -1,2 +1,5 @@
 // Create a function that takes two numbers as arguments and return their sum.
 const addition = (a, b) => a + b
+
+// Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
+const frames = (min, fps) => min * fps * 60
