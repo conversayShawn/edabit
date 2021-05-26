1875428f152b4e75e3dbbc2e043cf417fecfad4f
Shawn <shawn.harris3510@gmail.com>
2021-05-26 18:32:53 -0400

    Fuel Up

--- very_easy/app.js
+++ very_easy/app.js
@@ -23,3 +23,7 @@ const makePair = (num1, num2) => {
   }
 const makePair = (a,b) => [a,b]; // simple solution
 const makePair = (...a) => a; // refactor, spread syntax
+
+// A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
+// Create a function which calculates the amount of fuel it needs, given the distance.
+const calculateFuel = (n) => (n<=10) ? 100 : (n*10)
