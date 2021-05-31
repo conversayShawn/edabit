f56aefb9fb2601da2969cd1957f330f57847e221
Shawn <shawn.harris3510@gmail.com>
2021-05-31 14:37:43 -0400

    area of a rectangle

--- very_easy/app.js
+++ very_easy/app.js
@@ -79,3 +79,6 @@ const isEven = n => !(n % 2) // not operator
 
 // Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
 const divisibleByFive = (n) => n % 5 === 0
+
+// Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1.
+const area = (h, w) => h > 0 && w > 0 ? h * w : -1
