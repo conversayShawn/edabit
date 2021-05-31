be473fbc3533d6bb11e5e41e08a41f956fc628ae
Shawn <shawn.harris3510@gmail.com>
2021-05-30 21:24:53 -0400

    minimal: if boolean then boolean - correct

--- very_easy/app.js
+++ very_easy/app.js
@@ -73,3 +73,4 @@ const dividesEvenly = (a, b) => !(a % b) // not operator
 
 // In the Code tab you will find a code that is missing a single character in order to pass the tests. However, YOUR GOAL is to submit a function as minimalist as possible. Use the tips in the Tips section down below.
 // Write a function that returns true if the given integer is even, and false if it's odd.
+const isEven = (n) => n % 2 === 0 ? true : false
