7258b5952bb8d71d11eb3e7f214c24a16efb379e
Shawn <shawn.harris3510@gmail.com>
2021-06-01 23:08:27 -0400

    is string empty

--- very_easy/app.js
+++ very_easy/app.js
@@ -82,3 +82,6 @@ const divisibleByFive = (n) => n % 5 === 0
 
 // Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1.
 const area = (h, w) => h > 0 && w > 0 ? h * w : -1
+
+// Create a function that returns true if a string is empty and false otherwise.
+const isEmpty = (s) => s.length === 0
