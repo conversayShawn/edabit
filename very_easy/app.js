6aacbf36141bd570440dfa652e538960da65d060
Shawn <shawn.harris3510@gmail.com>
2021-06-02 22:14:34 -0400

    string to int

--- very_easy/app.js
+++ very_easy/app.js
@@ -103,3 +103,6 @@ const acceptIntoMovie = (a, iS) => a < 15 && !iS // && and !
 // A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 18 and older and when he's not on break.
 // Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
 const shouldServeDrinks = (a, oB) => a >= 18 && !oB
+
+// Create a function that takes a string and returns it as an integer.
+const stringInt = (str) => parseInt(str)
