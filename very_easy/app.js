4d51fff48afeeac64d0e2254628feb988d1e6cc0
Shawn <shawn.harris3510@gmail.com>
2021-06-05 23:26:51 -0400

    5 adder functions - global

--- very_easy/app.js
+++ very_easy/app.js
@@ -120,3 +120,4 @@ const add3 = (x) => x + 3
 const add5 = (x) => x + 5
 const add7 = (x) => x + 7
 const add11 = (x) => x + 11
+[2, 3, 5, 7, 11].forEach(n => global['add' + n] = (v) => n + v); // global variable
