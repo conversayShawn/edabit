9f274b8ffbfd4c0036be306107b5aa284d14a962
Shawn <shawn.harris3510@gmail.com>
2021-06-05 23:26:11 -0400

    5 adder functions

--- very_easy/app.js
+++ very_easy/app.js
@@ -112,3 +112,11 @@ const stringInt = (str) => Number
 
 // Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1
 const area = (h, w) => h && w > 0 ? h * w : -1
+
+// In the Code tab you will find code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+// Write five adder functions:
+const add2 = (x) => x + 2
+const add3 = (x) => x + 3
+const add5 = (x) => x + 5
+const add7 = (x) => x + 7
+const add11 = (x) => x + 11
