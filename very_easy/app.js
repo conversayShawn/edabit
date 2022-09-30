41fd3230e4dd30ec422deee9fa65691574892fd6
Shawn Harris <shawn.harris3510@gmail.com>
2022-09-29 19:58:27 -0700

    fix the cubes

---
+++ very_easy/app.js
@@ -0,0 +1,8 @@
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+function cubes(a) {
+	return a ** 3
+}
+const cubes = (a) => {
+	return a**3
+}
+cubes=(a)=>a**3
