08842705cfc69285e851fa962c08babcac481f6c
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-06 18:48:48 -0700

    makes 10

--- very_easy/app.js
+++ very_easy/app.js
@@ -212,3 +212,7 @@ function footballPoints(wins, draws, losses) {
 	footballPoints[0] * 3 + footballPoints[1]
 }
 return footballPoints
+
+// Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
+const makesTen = (a, b) => (a===10)||(b===10)||(a+b===10)?true:false
+const makesTen1 = (a, b) => [a,b,a+b].includes(10)
