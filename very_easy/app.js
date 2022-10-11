f78ab6528d9da60bcb92f675185c5e0768391d36
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-10 19:38:17 -0700

    drinks allowed

--- very_easy/app.js
+++ very_easy/app.js
@@ -363,3 +363,7 @@ const drop = (arr, val = 1) => {
 // They have parental supervision.
 // The function accepts two parameters, age and isSupervised. Return a boolean.
 const acceptIntoMovie = (a, s) => !(a<15) || s
+
+// A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 18 and older and when he's not on break.
+// Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
+const shouldServeDrinks = (a, b) => !(a<18 || b)
