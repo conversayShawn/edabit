8a0e715160cbd31bd93c7967d72d4e7986a70bc1
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-10 19:41:19 -0700

    ternary operator

--- very_easy/app.js
+++ very_easy/app.js
@@ -367,3 +367,6 @@ const acceptIntoMovie = (a, s) => !(a<15) || s
 // A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 18 and older and when he's not on break.
 // Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
 const shouldServeDrinks = (a, b) => !(a<18 || b)
+
+// Write a function that uses the ternary operator to return "yeah" if bool is true, and "nope" otherwise.
+const yeah_nope = (bool) => bool ? `yeah` : `nope`
