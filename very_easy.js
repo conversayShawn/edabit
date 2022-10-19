658a448efe6f164fbb943e4e56899348e870bab1
Shawn Harris <shawn.harris3510@gmail.com>
2022-10-19 16:16:32 -0700

    leap year

--- very_easy.js
+++ very_easy.js
@@ -425,3 +425,7 @@ function posCom(num) {
 function posCom(num) {
 	return 2 << (num - 1);
 }
+
+// A leap year happens every four years, so it's a year that is perfectly divisible by four. However, if the year is a multiple of 100 (1800, 1900, etc), the year must be divisible by 400.
+// Write a function that determines if the year is a leap year or not.
+const leapYear = (y) => !(y%4 || !y%400)
